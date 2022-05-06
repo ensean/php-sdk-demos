72709333171739a590fab185241f5af04e297105
liyxmse <liyxmse@amazon.com>
2022-05-06 10:06:09 +0800

    add rek detect moderationlabel demo

---
+++ rek_moderationLabels.php
@@ -0,0 +1,26 @@
+<?php
+
+require "./libs/aws.phar";
+use Aws\Rekognition\RekognitionClient;
+
+$rekClient = new RekognitionClient([
+    'profile' => 'default',
+    'region' => 'ap-southeast-1',
+    'version' => 'latest'
+]);
+
+$result = $rekClient->detectModerationLabels([
+    'Image' => [ // REQUIRED
+        'S3Object' => [
+            'Bucket' => 'liyx-media',
+            'Name' => 'imgs/detect-4.jpeg', # without first /
+            // 'Version' => '<string>', ignore for bucket witout versioning
+        ],
+    ],
+    'MinConfidence' => 80,  // control confidence, 50 - 100
+
+]);
+
+foreach ($result['ModerationLabels'] as $label) {
+    echo $label['Name'] . ": " . $label['Confidence'] . "\n";
+}
